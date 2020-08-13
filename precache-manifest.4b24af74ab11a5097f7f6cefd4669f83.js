7e5be3e1dcb19a7455a3509250a0d6644fdb62f2
harshadaram <sriharsha.0722@gmail.com>
2020-08-13 00:00:23 -0400

    Updates

---
+++ precache-manifest.4b24af74ab11a5097f7f6cefd4669f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e1ee1ee92520d2993ced8307ef21ea",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dfbd0f5483b2765e8233",
+    "url": "/monsters-rolodex/static/css/main.803c5003.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbd0f5483b2765e8233",
+    "url": "/monsters-rolodex/static/js/main.3a3d78b8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
